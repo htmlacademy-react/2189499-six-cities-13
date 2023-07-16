816282b4585cc98996d6a0bc6640ec695e6120c9
Alex <collagememorycollage@gmail.com>
2023-07-16 23:39:35 +0300

    реализовал роутинг и проверку авторизации пользователя

---
+++ src/components/private-route.tsx
@@ -0,0 +1,19 @@
+import { Navigate } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../const.tsx';
+
+type PrivateRouteStatusProps = {
+  authorizationStatus: AuthorizationStatus;
+  children: JSX.Element;
+}
+
+function PrivateRoute(props: PrivateRouteStatusProps): JSX.Element {
+  const { authorizationStatus, children } = props;
+
+  return (
+    authorizationStatus === AuthorizationStatus.Auth
+      ? children
+      : <Navigate to={AppRoute.Login} />
+  );
+}
+
+export default PrivateRoute;
---
+++ src/const.tsx
@@ -0,0 +1,18 @@
+const CountPlaces = {
+  Count: 5,
+} as const;
+
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/:id',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
+
+export default CountPlaces;
